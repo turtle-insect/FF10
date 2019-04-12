3da4c8a4bed8d0d381d519e3d7f449d36a64ec40
turtle-insect <turtle.insect@gmail.com>
2019-04-13 05:36:54 +0900

    first commit

---
+++ FF10/Charactor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF10
+{
+	class Charactor
+	{
+		private readonly uint mAddress;
+
+		public Charactor(uint address)
+		{
+			mAddress = address;
+		}
+
+		public uint HP
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 2, 4); }
+			set
+			{
+				Util.WriteNumber(mAddress + 2, 4, value, 0, 99999);
+				Util.WriteNumber(mAddress + 26, 4, value, 0, 99999);
+				Util.WriteNumber(mAddress + 34, 4, value, 0, 99999);
+			}
+		}
+
+		public uint MP
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 6, 4); }
+			set
+			{
+				Util.WriteNumber(mAddress + 6, 4, value, 0, 99999);
+				Util.WriteNumber(mAddress + 30, 4, value, 0, 99999);
+				Util.WriteNumber(mAddress + 38, 4, value, 0, 99999);
+			}
+		}
+
+		public uint Lv
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 57, 1); }
+			set { Util.WriteNumber(mAddress + 57, 1, value, 0, 99); }
+		}
+
+		public uint Attack
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 10, 1); }
+			set { Util.WriteNumber(mAddress + 10, 1, value, 0, 255); }
+		}
+
+		public uint Defense
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 11, 1); }
+			set { Util.WriteNumber(mAddress + 11, 1, value, 0, 255); }
+		}
+
+		public uint Magic
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 12, 1); }
+			set { Util.WriteNumber(mAddress + 12, 1, value, 0, 255); }
+		}
+
+		public uint MagicGuard
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 13, 1); }
+			set { Util.WriteNumber(mAddress + 13, 1, value, 0, 255); }
+		}
+
+		public uint Speed
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 14, 1); }
+			set { Util.WriteNumber(mAddress + 14, 1, value, 0, 255); }
+		}
+
+		public uint Luck
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 15, 1); }
+			set { Util.WriteNumber(mAddress + 15, 1, value, 0, 255); }
+		}
+
+		public uint Avoidance
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 16, 1); }
+			set { Util.WriteNumber(mAddress + 16, 1, value, 0, 255); }
+		}
+
+		public uint Hit
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 17, 1); }
+			set { Util.WriteNumber(mAddress + 17, 1, value, 0, 255); }
+		}
+	}
+}
---
+++ FF10/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace FF10
+{
+	class ViewModel
+	{
+		public ObservableCollection<Charactor> Party { get; set; } = new ObservableCollection<Charactor>();
+
+		public ViewModel()
+		{
+			for(uint i = 0; i < 7; i++)
+			{
+				Party.Add(new Charactor(0x5616 + i * 148));
+			}
+		}
+
+		public uint GIL
+		{
+			get { return SaveData.Instance().ReadNumber(0x3D90, 4); }
+			set { Util.WriteNumber(0x3D90, 4, value, 0, 9999999); }
+		}
+	}
+}
